5f9351de70dc311f5c2df1d0e09f806dc047394d
Nikola Stojanovic <35263124+generalni@users.noreply.github.com>
2018-11-04 16:56:08 +0100

    novi dizajn

--- js/render.js
+++ js/render.js
@@ -1,38 +1,16 @@
 var head = '<div class="container">\
               <div class="logo">\
                 <a href="/index.html">\
-                  <span>popravke & iskustva</span>\
-                  <span class="text-highlight">blog</span>\
+                  <span>KAKO POPRAVITI\
+                  <span class="text-highlight">BLOG</span></span>\
+                </a>\
+              </div>\
+              <div class="youtube">\
+                <a href="https://www.youtube.com/channel/UCsORJh7ogpaDdOSJgCVpEZQ">\
+                  <img src="../../img/youtube.png">\
                 </a>\
               </div>\
             </div>'
-
-var right_side = '<!-- YOUTUBE -->\
-					\
-          <div class="box">\
-            <a href="https://www.youtube.com/channel/UCsORJh7ogpaDdOSJgCVpEZQ">\
-              <h2>Pogledajte moj Youtube kanal</h2>\
-              <span>Slika govori više od 1000 reči. Video govori 25 slika u sekundi.</span>\
-            </a>\
-          </div>\
-					\
-          <!-- OLD POSTS -->\
-					\
-          <div class="box">\
-            <h2>Stari tekstovi</h2>\
-            <hr>\
-            <a href="/articles/uknjizba-katastar/uknjizba-katastar.html">\
-              <span>Uknjižba stana u katastru Savski venac</span>\
-            </a>\
-            <hr>\
-            <a href="/articles/otvaranje-vrata-mobilnim/otvaranje-vrata-mobilnim.html">\
-              <span>Otvaranje električne brave mobilnim telefonom</span>\
-            </a>\
-            <hr>\
-            <a href="/articles/kako-popraviti-toster/kako-popraviti-toster.html">\
-              <span>Popravka tostera, zamena termostata</span>\
-            </a>\
-          </div>'
 
   var foot = '<div class="container">\
                 <span>\
@@ -43,6 +21,5 @@ var right_side = '<!-- YOUTUBE -->\
 
 function render() {
   $("header").html(head);
-	$("aside").html(right_side);
   $("footer").html(foot);
 }
